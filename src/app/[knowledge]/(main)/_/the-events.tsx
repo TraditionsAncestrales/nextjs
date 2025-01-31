b9dc33c764ce8e2baa41ea20ec63e44ad1edddcb
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-01-31 11:42:48 +0400

    Update the-events.tsx

--- src/app/[knowledge]/(main)/_/the-events.tsx
+++ src/app/[knowledge]/(main)/_/the-events.tsx
@@ -3,18 +3,18 @@
 import { RecordsItems } from "@/components/records-items";
 import type { Item } from "@/lib/pocketbase/utils";
 import type { Intent } from "@/styles/ui";
-import { useLayoutEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 // MAIN ************************************************************************************************************************************
 export function TheEvents({ intent, items }: TheEventsProps) {
-  const [events, setEvents] = useState<Item[]>([]);
+  // const [events, setEvents] = useState<Item[]>([]);
 
-  useLayoutEffect(() => {
-    setEvents(items.filter(({ stale }) => stale && stale >= new Date().toISOString()));
-  }, [items]);
+  // useEffect(() => {
+  //   setEvents(items.filter(({ stale }) => stale && stale >= new Date().toISOString()));
+  // }, [items]);
 
   return (
-    <RecordsItems title="Événement" items={events} border="bottom" intent={intent}>
+    <RecordsItems title="Événement" items={items} border="bottom" intent={intent}>
       <div>
         <p>Retrouvez bientôt ici l&apos;ensemble de mes événements.</p>
         <strong>Vous pouvez déjà en obtenir le programme en me faisant une demande via le formulaire de contact.</strong>
